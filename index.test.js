44174a678a876a500510bdaa485f512491cfd3df
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-15 18:30:45 +0530

    added test for not-found route

--- index.test.js
+++ index.test.js
@@ -18,3 +18,17 @@ test('Server is up and running and responds to ping requets', async (t) => {
     t.fail();
   }
 });
+
+test('Server responds with 404 to a non-existant api endpoint', async (t) => {
+  const microInstance = micro(Service);
+  const url = await listen(microInstance);
+  await request(`${url}/non-existent-route`)
+    .then(() => false, (res) => {
+      const response = JSON.parse(res.response.body);
+      if (res.statusCode === 404 && response.status === 'fail') {
+        t.pass();
+      } else {
+        t.fail();
+      }
+    });
+});
